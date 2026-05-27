e128a45015e59da4395b9c86b9d9d0455cbb7d53
BNMWAURA <88mwaurabn@gmail.com>
2026-05-27 05:56:38 -0700

    Set up Express app with middleware and routes

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,3 +1,11 @@
+import express, { type Express } from "express";
+import cors from "cors";
+import pinoHttp from "pino-http";
+import router from "./routes";
+import { logger } from "./lib/logger";
+
+const app: Express = express();
+
 const loggerMiddleware = pinoHttp({
   logger,
   serializers: {
@@ -15,3 +23,12 @@ const loggerMiddleware = pinoHttp({
     },
   },
 });
+
+app.use(loggerMiddleware);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+export default app;
