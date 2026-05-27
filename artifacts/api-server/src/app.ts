79cf4297e914ef3aa1cc69e125f434f9ea965c3e
BNMWAURA <88mwaurabn@gmail.com>
2026-05-27 06:03:26 -0700

    Refactor app.ts with type annotations and imports
    Updated import statements and added type annotations for request and response in logger middleware.

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
-import express, { type Express } from "express";
+import express, { type Express, Request, Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttp = require("pino-http");
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -9,14 +9,14 @@ const app: Express = express();
 const loggerMiddleware = pinoHttp({
   logger,
   serializers: {
-    req(req) {
+    req(req: any) {
       return {
         id: req.id,
         method: req.method,
         url: req.url?.split("?")[0],
       };
     },
-    res(res) {
+    res(res: any) {
       return {
         statusCode: res.statusCode,
       };
