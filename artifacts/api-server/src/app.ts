2fe6332dfcdc3e4df84f2dcabd8ae348f0d2144d
BNMWAURA <88mwaurabn@gmail.com>
2026-05-27 05:50:08 -0700

    Refactor logger middleware setup in app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,35 +1,17 @@
-import express, { type Express } from "express";
-import cors from "cors";
-import * as pinoHttp from "pino-http";
-import router from "./routes";
-import { logger } from "./lib/logger";
-
-const app: Express = express();
-
-app.use(
-  pinoHttp.default({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+const loggerMiddleware = pinoHttp({
+  logger,
+  serializers: {
+    req(req) {
+      return {
+        id: req.id,
+        method: req.method,
+        url: req.url?.split("?")[0],
+      };
     },
-  }),
-);
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", router);
-
-export default app;
+    res(res) {
+      return {
+        statusCode: res.statusCode,
+      };
+    },
+  },
+});
