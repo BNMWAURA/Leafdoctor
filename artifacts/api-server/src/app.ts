40ebe9107b988ba877d4e0195b7be2cbc1ef9c84
BNMWAURA <88mwaurabn@gmail.com>
2026-05-28 00:04:34 -0700

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import router from "./routes";
-import { logger } from "./lib/logger";
+import { logger } from "./lib/logger.js";
 const pinoHttp = require("pino-http");
 const app: Express = express();
 const loggerMiddleware = pinoHttp({
